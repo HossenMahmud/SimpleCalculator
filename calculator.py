b4d09d0246fd6d8095a46db69c6065bda0f132b2
himel <himelmahmud44@gmail.com>
2020-12-23 12:07:27 +0600

    This is select option

--- calculator.py
+++ calculator.py
@@ -6,6 +6,12 @@ def subtract(x, y):
 
 def multiply(x, y):
     return x * y
-    
+
 def divide(x, y):
     return x / y
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
